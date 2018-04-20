386f1750d05a0ade5dc9877fb0d126b3a3b41106
Harco Kuppens <h.kuppens@cs.ru.nl>
2018-04-20 12:00:59 +0200

    added  recipe for 0.6 release

--- Formula/torxakis.rb
+++ Formula/torxakis.rb
@@ -2,20 +2,20 @@
 class Torxakis < Formula
   desc "Tool for Model Based Testing"
   homepage "https://github.com/TorXakis/TorXakis"
-  url "https://github.com/TorXakis/TorXakis/archive/v0.5.0.tar.gz"
-  sha256 "03b370d2145206769e6cd12935404c5235a2afa6a779aef0aa3d0030057c6375"
+  url "https://github.com/TorXakis/TorXakis/archive/v0.6.0.tar.gz"
+  sha256 "c4c9e668580528dabd07f99d0c5534905cebdcf670e6ff1901be77a8a0dc9cfc"
   head "https://github.com/TorXakis/TorXakis.git"
 
   bottle do
     cellar :any_skip_relocation
-    root_url "https://github.com/TorXakis/TorXakis/releases/download/v0.5.0/"
-    sha256 "1f8a845a2e8c3622a5d6590aa07fc1f44dee1d1315216f823feb41850be388af" => :high_sierra
-    sha256 "e8ef8d4f47bb081fffa3d8b7bf25535fd2c6963df38ecfd7a2c8e3676c87ffe8" => :sierra
-    sha256 "139c428b838f459ebeef7ea1984a26280cae1f0749e44ec0888f766b9f61877c" => :el_capitan
+    root_url "https://github.com/TorXakis/TorXakis/releases/download/v0.6.0/"
+    sha256 "98ad4d2fd0bce23a86555cc4464593fff816e295f44b08c2a52e0e1fc904d86a" => :high_sierra
+    sha256 "83fd14a4d4b4b8d68ea3fdd580a88897265ae7cb8246241168860f155aff1bbb" => :sierra
+    sha256 "d7e31605795b25dddfce047acf413b19974c83b6b3a1a58d4925a2beddb49c9a" => :el_capitan
   end
 
   depends_on "haskell-stack" => :build
-  depends_on "z3" => :run
+  depends_on "z3"
 
   def install
     ohai "running install"
