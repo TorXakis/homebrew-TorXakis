f905ab03acc5296e2e68e64c31e4a63493dc67f0
Harco Kuppens <h.kuppens@gmail.com>
2019-08-18 01:07:39 +0200

    Update torxakis.rb

--- Formula/torxakis.rb
+++ Formula/torxakis.rb
@@ -2,8 +2,8 @@
 class Torxakis < Formula
   desc "Tool for Model Based Testing"
   homepage "https://github.com/TorXakis/TorXakis"
-  url "https://github.com/TorXakis/TorXakis/archive/v0.7.0.tar.gz"
-  sha256 "46a78b4303b170aa37f8da36bbe2e8ff27952575e98dcca608e1123a8d74c5d0"
+  url "https://github.com/TorXakis/TorXakis/archive/v0.8.1.tar.gz"
+  sha256 "72079ddb7ca4a6ea6132141de4d19ba88b9b6c1c326aca228a6086f938f19b22"
   head "https://github.com/TorXakis/TorXakis.git"
 
   depends_on "haskell-stack" => :build
@@ -11,16 +11,24 @@ class Torxakis < Formula
 
   bottle do
     cellar :any_skip_relocation
-    root_url "https://github.com/TorXakis/TorXakis/releases/download/v0.7.0/"
-    sha256 "39f8cccbf22a9c3ac0d6c6f683532cecc5b61a7068ca2abee9868210d3473fcd" => :high_sierra
-    sha256 "adeeee80220f393cb87a0bb132a56b69922380c64aa78b23350feafa29b74f8b" => :sierra
-    sha256 "d2bdedcbeeecdc21fdae691edb87edec6599eede460fdd62b2966759639af5b0" => :el_capitan
+    root_url "https://github.com/TorXakis/TorXakis/releases/download/v0.8.1/"
+    
+    sha256 "bbd44f31cca791975c1d384b7ba6735df8569e566ec8b00e67c6af26c23e2f2d" => :mojave
+    sha256 "9fa44d885f38da6620757b43ddb5bc65e2a4e0dfe8bb710d624ad115efb6c4b7" => :high_sierra
+    sha256 "581aa85c14b3e10b2329d9d54c416e59386672d17bac20ef3bb53a2505d55528" => :sierra
+    
   end
-
+    
   def install
     ohai "running install"
     jobs = ENV.make_jobs
     ENV.deparallelize
+
+    # next line is only needed for v0.8.1 to patch a mistake in the stack yaml
+    # file; after this release it was already fixed in the Torxakis repository
+    # So for future recipes the next line may be removed.
+    system "sed", "-i", "-e", "s/^- location: /- /", "stack_linux.yaml"
+    
     system "stack", "-j#{jobs}", "--stack-yaml=stack_linux.yaml", "setup"
     system "stack", "-j#{jobs}", "--stack-yaml=stack_linux.yaml", "--local-bin-path=#{bin}", "install"
     prefix.install "examps"
