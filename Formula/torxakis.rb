99754fd24600bae83c253cfdb7df883bd2f53007
Harco Kuppens <h.kuppens@gmail.com>
2021-12-06 00:11:30 +0100

    fixed syntax for torxakis recipe for newer homebrew version
    used the tool:
      $ brew style --fix ./torxakis.rb
    which did some automatic fixes,
    and mentioned some fixes which I could do manually

--- Formula/torxakis.rb
+++ Formula/torxakis.rb
@@ -1,3 +1,7 @@
+# typed: false
+# frozen_string_literal: true
+
+# torxakis: a tool for model-based testing
 class Torxakis < Formula
   desc "Tool for Model Based Testing"
   homepage "https://github.com/TorXakis/TorXakis"
@@ -5,17 +9,15 @@ class Torxakis < Formula
   sha256 "2629a602c1dc33224336cffc05292484951233f624a64b04648f947dba6c0d94"
   head "https://github.com/TorXakis/TorXakis.git"
 
-  depends_on "haskell-stack" => :build
-  depends_on "z3"
-
   bottle do
-        cellar :any_skip_relocation
-        root_url "https://github.com/TorXakis/homebrew-TorXakis/releases/download/v0.9.0/"
-        sha256 "cf324a1c404ddf083e55e55b907a77371ca9f91ca4f2b20bc5434ebbeb3d923e" => :catalina
-        sha256 "dbb245251780eaeb96987754b3cfa780c9fa4bce541ffd20984d7eb73fe195f9" => :mojave
-        sha256 "72c26afb6c1129038063b51d76bc0566d67c337402b378e2f50b1b766e7a4965" => :high_sierra
+    root_url "https://github.com/TorXakis/homebrew-TorXakis/releases/download/v0.9.0/"
+    sha256 cellar: :any_skip_relocation, catalina:    "cf324a1c404ddf083e55e55b907a77371ca9f91ca4f2b20bc5434ebbeb3d923e"
+    sha256 cellar: :any_skip_relocation, mojave:      "dbb245251780eaeb96987754b3cfa780c9fa4bce541ffd20984d7eb73fe195f9"
+    sha256 cellar: :any_skip_relocation, high_sierra: "72c26afb6c1129038063b51d76bc0566d67c337402b378e2f50b1b766e7a4965"
   end
 
+  depends_on "haskell-stack" => :build
+  depends_on "z3"
 
   def install
     ohai "running install"
